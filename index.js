0c10cd8f18c087e75904f00dec073d4453b088cb
Toan Bao <baotoandd2016@gmail.com>
2023-06-16 21:45:59 +0700

    update

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(
 );
 
 const url = 'https://muachung365.vn/?utm_campaign=18036050079&gad=1'
+const characterUrl = "https://muachung365.vn/"
 
 
 
@@ -39,13 +40,15 @@ app.get("/v1", async (req, resp) => {
           const priceCurrent = $(this).find(".price > ins ").text().trim()
           const priceSale = $(this).find(".price > del ").text().trim()
           const detail = $(this).find("a").attr("href")
+          const url = "https://data-product.onrender.com/v1/" + detail.split("https://muachung365.vn/")[1]
 
           thumbnails.push({
             title,
             image,
             priceCurrent,
             priceSale,
-            detail
+            detail,
+            url
           })
         });
 
@@ -67,6 +70,46 @@ app.get("/v1", async (req, resp) => {
     resp.status(500).json(error);
   }
 });
+
+
+app.get("/v1/:nameproduct", (req,resp) => {
+  let url = characterUrl + req.params.nameproduct
+
+  let fullImage = []
+  let title = ''
+  let detailProduct = []
+
+
+  try {
+    axios(url).then((res) => {
+        const html = res.data
+        const $ = cheerio.load(html)
+
+        $('.product-detail__title', html).each(function(){
+          title = $(this).text().trim()
+        }) 
+
+        $('.thumb', html).each(function(i,e){
+          const gallery = $(e).find("img").attr("src");
+          fullImage.push(gallery);
+        })
+          detailProduct.push({
+            title,
+            fullImage
+        })
+  
+
+        resp.status(200).json(detailProduct)
+    }) 
+    .catch((error) => {
+      console.log(error);
+      resp.status(500).json(error);
+    });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json(error)
+  }
+}) 
 
 
 
